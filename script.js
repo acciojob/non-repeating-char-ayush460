6a681c832050bce635bfc0fa80978580dd8f09d3
ayush460 <90319070+ayush460@users.noreply.github.com>
2023-04-16 23:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function firstNonRepeatedChar(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+    if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
+      return char;
+    }
+  }
+  return null;
+}
+
+let input = prompt("Enter a string:");
+let result = firstNonRepeatedChar(input);
+
+if (result) {
+  alert("The first non-repeated character is: " + result);
+} else {
+  alert("There are no non-repeated characters in the string.");
+}
